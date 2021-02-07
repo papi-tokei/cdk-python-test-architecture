83985fc1c05b9ce44422636fd1d89d36cfc12e91
Hiroya Akita <akky.develop@gmail.com>
2021-02-08 01:08:37 +0900

    fix: update test code

--- resources/get_data/test_get_data.py
+++ resources/get_data/test_get_data.py
@@ -1,21 +1,34 @@
-import dataclasses
+from . import index
+from .index import handler
+import user_table
+from user_table import UserTable
+from decimal import Decimal
 
 
-def test_get_data(monkeypatch):
+def test_get_data(dynamodb, monkeypatch):
+    # print('aaa')
+    # print(__name__)
+    # print('hello')
 
-    print('aaa')
-    print(__name__)
-    print('hello')
-    from . import index
-    from .index import handler
-    from user_table import UserTable
+    # def mock_return(_table_name):
+    #     return 'aaa'
 
-    def mock_return(table_name):
-        return 'aaa'
+    # class Tracer:
+    #     def __init__(self, serice):
+    #         pass
 
-    def scan_user_data():
-        return 'dummy data dayo'
+    # monkeypatch.setattr('aws_lambda_powertools.Tracer', Tracer)
+    monkeypatch.setattr(UserTable, "table", dynamodb.Table('sample_table1_test'))
+    UserTable.create_table(dynamodb)
+    UserTable.create_user('taro', 12)
+    UserTable.create_dummy_data(
+        [
+            {'id': '1234', 'name': 'taro', 'age': Decimal('12')},
+            {'id': '567', 'name': 'taro', 'age': Decimal('12')},
+            {'id': '666', 'name': 'taro', 'age': Decimal('12')},
+        ]
+    )
 
-    monkeypatch.setattr(index, "get_table_data", mock_return)
-    monkeypatch.setattr(UserTable, "scan_user_data", scan_user_data)
+    data = UserTable.scan_user_data()
+    print(data)
     handler('', '')
