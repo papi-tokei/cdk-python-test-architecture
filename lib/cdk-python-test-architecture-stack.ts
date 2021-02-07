80b57515311823a7b5d8b98eefdb9b5440093ea9
Hiroya Akita <akky.develop@gmail.com>
2021-02-08 00:22:37 +0900

    fix: update stack code

--- lib/cdk-python-test-architecture-stack.ts
+++ lib/cdk-python-test-architecture-stack.ts
@@ -1,7 +1,13 @@
 import * as cdk from "@aws-cdk/core";
 import { Table, AttributeType } from "@aws-cdk/aws-dynamodb";
 import { RemovalPolicy } from "@aws-cdk/core";
-import { Function, Runtime, Code, LayerVersion } from "@aws-cdk/aws-lambda";
+import {
+  Function,
+  Runtime,
+  Code,
+  LayerVersion,
+  Tracing,
+} from "@aws-cdk/aws-lambda";
 import * as path from "path";
 
 export class CdkPythonTestArchitectureStack extends cdk.Stack {
@@ -15,7 +21,9 @@ export class CdkPythonTestArchitectureStack extends cdk.Stack {
       compatibleRuntimes: [Runtime.PYTHON_3_8],
       description: "Layer sample",
     });
-    const customLayerResource = customLayer.node.findChild('Resource') as cdk.CfnResource;
+    const customLayerResource = customLayer.node.findChild(
+      "Resource"
+    ) as cdk.CfnResource;
     customLayerResource.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN);
 
     const requestsLayer = new LayerVersion(this, "MyLayer2", {
@@ -24,15 +32,34 @@ export class CdkPythonTestArchitectureStack extends cdk.Stack {
       compatibleRuntimes: [Runtime.PYTHON_3_8],
       description: "requests layer",
     });
-    const requestsLayerResource = customLayer.node.findChild('Resource') as cdk.CfnResource;
+    const requestsLayerResource = requestsLayer.node.findChild(
+      "Resource"
+    ) as cdk.CfnResource;
     requestsLayerResource.applyRemovalPolicy(cdk.RemovalPolicy.RETAIN);
+
+    const awsLambdaPowerToolsLayer = new LayerVersion(this, "MyLayer3", {
+      layerVersionName: "aws-lambda-powertools",
+      code: Code.fromAsset("layer_builder/aws-lambda-powertools"),
+      compatibleRuntimes: [Runtime.PYTHON_3_8],
+      description: "aws-lambda-powertools layer",
+    });
+    const awsLambdaPowerToolsLayerResource = awsLambdaPowerToolsLayer.node.findChild(
+      "Resource"
+    ) as cdk.CfnResource;
+    awsLambdaPowerToolsLayerResource.applyRemovalPolicy(
+      cdk.RemovalPolicy.RETAIN
+    );
 
     // The code that defines your stack goes here
     const fn = new Function(this, "MyFunction", {
       runtime: Runtime.PYTHON_3_8,
       handler: "index.handler",
       code: Code.fromAsset(path.join(__dirname, "../resources/get_data")),
-      layers: [customLayer, requestsLayer],
+      tracing: Tracing.ACTIVE,
+      layers: [customLayer, requestsLayer, awsLambdaPowerToolsLayer],
+      environment:{
+        "LOG_LEVEL": "INFO"
+      }
     });
     const table1 = new Table(this, "SampleTable1", {
       tableName: "sample_table1",
